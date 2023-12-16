48f8ef40f2a23d1284e53fb6416c9aec05bd52a7
MrGummyl3ear <aliko.beraya@gmail.com>
2023-12-16 12:41:37 +0300

    solve sem7

--- src/seminar6.py
+++ src/seminar6.py
@@ -33,7 +33,24 @@ def download_data():
             zip_ref.extractall(PATH_TO_DATA)
     else:
         print('Data is already extracted!')
+    num_skipped = 0
+    for folder_name in ("Cat", "Dog"):
+      folder_path = os.path.join(PATH_TO_DATA+"/PetImages", folder_name)
+      for fname in os.listdir(folder_path):
+        fpath = os.path.join(folder_path, fname)
+        try:
+            #print(fpath)
+            fobj = open(fpath, "rb")
+            is_jfif = b"JFIF" in fobj.peek(10)
+        finally:
+            fobj.close()
 
+        if not is_jfif:
+            num_skipped += 1
+            # Delete corrupted image
+            os.remove(fpath)
+
+      print(f"Deleted {num_skipped} images.")
 
 def make_model(input_shape, num_classes):
     inputs = keras.Input(shape=input_shape)
@@ -78,7 +95,7 @@ def train():
     """Pipeline: Build, train and save model to models/model_6"""
     # Todo: Copy some code from seminar5 and https://keras.io/examples/vision/image_classification_from_scratch/
     print('Training model')
-    epochs = 1
+    epochs = 3
     batch_size = 64
     image_size = (180, 180)
 
